d9093a0b96ef8f8112f121994766d34dba9d7a13
Luisa Eysele <eysele.luisa@gmail.com>
2023-08-18 17:06:08 -0600

    ci: Uso de bootstrap

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,22 @@
 console.log("Sesión JS09 NPM y Axios");
+
+const apiFetch = document.getElementById("apiFetch");
+const apiAxios = document.getElementById("apiAxios");
+
+apiFetch.onclick = async () =>{
+    console.log("botón apiFetch");
+    const responseJson = await fetch("https://fakestoreapi.com/products");
+    const response = await responseJson.json();
+    console.log(response);
+}
+
+/**
+ * Solicitud HTTP usando Axios
+ */
+apiAxios.onclick = async () =>{
+    console.log("botón apiFetch");
+    const response = await axios.get("https://fakestoreapi.com/products");
+    console.log(response);
+}
+
+
